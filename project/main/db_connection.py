8f7e0332dd08cacb6b5e0d83d45c62cee4e77050
Jojo <joeljeny@gmail.com>
2024-09-28 20:39:06 -0400

    removed mongodb

--- project/main/db_connection.py
+++ project/main/db_connection.py
@@ -1,20 +1,20 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
 
-import environ
+# import environ
 
-env = environ.FileAwareEnv()
-environ.Env.read_env()
+# env = environ.FileAwareEnv()
+# environ.Env.read_env()
 
-uri = env('DB_URL')
+# uri = env('DB_URL')
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
 
